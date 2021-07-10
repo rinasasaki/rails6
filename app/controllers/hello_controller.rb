b2ac27d086b9151696cecc6f6c0245a014317469
佐々木　莉奈 <1072sasaki@gmail.com>
2021-07-11 04:14:58 +0900

    [fix] hello controller

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,8 +1,8 @@
 class HelloController < ApplicationController
 
   def index
-    if params ['msg'] !=nil then
-      msg = 'Hello, ' + params ['msg'] + '!'
+    if params['msg'] !=nil then
+      msg = 'Hello, ' + params['msg'] + '!'
     else
       msg = 'this is sample page.'
   end
