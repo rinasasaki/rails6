e93b4d3c606f24b0932a91a2b269d10e6cf06d01
佐々木　莉奈 <1072sasaki@gmail.com>
2021-07-11 04:05:29 +0900

    [add] hello controller

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,15 +1,10 @@
 class HelloController < ApplicationController
 
   def index
-    msg = '
-    <html>
-    <body>
-      <h1>Sample Page</h1>
-      <p>this is Rails sample page!</p>
-    </body>
-    </html>
-      '
-      render html: msg.html_safe
+    if params ['msg'] !=nil then
+      msg = 'Hello, ' + params ['msg'] + '!'
+    else
+      msg = 'this is sample page.'
   end
 
 end
