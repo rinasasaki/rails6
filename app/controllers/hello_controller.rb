367cd07fe2c31be37b5329fa771f424bd3633f4e
佐々木　莉奈 <1072sasaki@gmail.com>
2021-08-14 22:22:42 +0900

    [fix] hello controller routes index.html

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,15 +1,17 @@
 class HelloController < ApplicationController
-  protect_from_forgery
+
   def index
-    if request.post? then 
+    if request.post? then
       @title = 'Result'
       @msg = 'you typed: ' + params['input1'] + '.'
-      @value = params ['input1']
+      @value = params['input1']
     else
-      @title = 'index'
+      @title = 'Index'
       @msg = 'type text...'
       @value = ''
     end
   end
 
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   post 'hello', to: 'hello#index'
-  post 'hello/index'
+  post 'hello/index'  
 end
