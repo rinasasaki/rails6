b99fa551f5ab6bce45813e196d0df3766ced58b2
佐々木　莉奈 <1072sasaki@gmail.com>
2021-08-16 21:47:15 +0900

    [fix] hello controller

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -4,9 +4,10 @@ class HelloController < ApplicationController
     if request.post? then
       @title = 'Result'
       if params['s1'] then
-        @msg = 'you selected: ' + params['s1']
+        @msg = 'you selected: ' 
         for val in params['s1']
           @msg += val + ''
+        end
       else
         @msg = 'not selected...'
       end
