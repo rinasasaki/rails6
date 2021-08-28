fcf8c7f01c6d472a29b0ae80c5581898f36037b0
佐々木　莉奈 <1072sasaki@gmail.com>
2021-08-28 20:19:26 +0900

    [fix] routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   get 'people/index'
+  get 'people', to: 'people#index'
   get 'msgboard/index'
   get 'hello/index'
   get 'hello', to: 'hello#index'
   post 'hello', to: 'hello#index'
-  post 'hello/index'  
+  post 'hello/index'
   get 'msgboard', to: 'msgboard#index'
   post 'msgboard', to: 'msgboard#index'
   get 'msgboard/index'
   post 'msgboard/index'
-  get 'people', to: 'people#index'
-  get 'people/:id', to: 'people#show'
   get 'people/add'
-  post 'people/add', to: 'people#create'　
+  post 'people/add', to: 'people#create'
+  get 'people/:id', to: 'people#show'
 end
