3b363cfa459b6a37d41d370253c42f80bbd733c8
佐々木　莉奈 <1072sasaki@gmail.com>
2021-08-28 11:43:07 +0900

    [fix] routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get 'hello', to: 'hello#index'
   post 'hello', to: 'hello#index'
   post 'hello/index'  
+  get 'msgboard', to: 'msgboard#index'
+  post 'msgboard', to: 'msgboard#index'
+  get 'msgboard/index'
+  post 'msgboard/index'
 end
