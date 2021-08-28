13a7ae76c8955a0af0bf14022bda4f77cafe11b6
佐々木　莉奈 <1072sasaki@gmail.com>
2021-08-28 20:27:55 +0900

    [fix] routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,18 +10,7 @@ Rails.application.routes.draw do
   post 'msgboard', to: 'msgboard#index'
   get 'msgboard/index'
   post 'msgboard/index'
-<<<<<<< HEAD
-  get 'people/add'
-  post 'people/add', to:'people#create'
-=======
-<<<<<<< HEAD
   get 'people/add'
   post 'people/add', to: 'people#create'
-=======
-  get 'people', to: 'people#index'
-  get 'people/add'
-  post 'people/add', to: 'people#create'　
->>>>>>> 803c2b063875d5d63a7a18d6b44e8fb563144be5
->>>>>>> 1b94b9f019d870da7b30133bcedb91611e9f0892
   get 'people/:id', to: 'people#show'
 end
